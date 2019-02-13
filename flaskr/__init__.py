12fc73e9782786d1196e5a5c387d43efe97f991f
MikeEngerer <michael.engerer@hotmail.com>
2019-02-12 20:19:40 -0500

    .flaskenv, db, schema, basic auth

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,4 +29,14 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    @app.route('/about')
+    def about():
+        return 'about page'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
